313297d67998fde5b41ecdcd292bc0552a608be4
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-01-19 18:04:15 +0700

    ADD A SETUP FOR LEARNING SPACE BUN

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+        <div className="content">
+            <h1>Hello World!</h1>
+        </div>
+        <div className="scroll">
+            <div className="block"></div>
+            <div className="block"></div>
+            <div className="block"></div>
+        </div>
+        <div className="content">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
